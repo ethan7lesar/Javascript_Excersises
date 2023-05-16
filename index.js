717b83b0c5a1400c0f019b4f835b0957de095d59
ethan7lesar <ethanlesar11@gmail.com>
2023-05-16 10:22:13 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+
+function myInterestrate(){
+
+    console.log('Hello World');
+    let ansr = 2 + 2;
+    alert('This is the browsers alert')
+    
+    let myName = "Ethan";
+    console.log(myName)
+    console.log(ansr);
+    const interestRate = 0.3;
+    console.log(interestRate);
+
+};
+
+
+//Connecting with button function 
+function myPerson() {
+
+    const person={
+        firstName:'Timothy', 
+        surname:'Barry',
+        occupation:'Idiot',
+        age:'12'
+
+};
+
+
+let {firstName,surname,occupation,age}=person;
+console.log(firstName);
+console.log(surname);
+console.log(occupation);
+console.log(age)
+console.log(person);
+
+alert("Check the console" + person);
+
+};
+
+//function myFunction(){
+
+let a = 10;
+let b = a;
+let c = b;
+document.write("The value of a is :", a  + "<br/>");
+document.write("The value of b is :", b + "<br/>");
+document.write("The value of c is : ", c + "<br/>");
+
+//};
+
+// name = ‘John’
+// age = 30
+// isUser = true
+// firstName: undefined
+// selectedColor : null
+
+
+
